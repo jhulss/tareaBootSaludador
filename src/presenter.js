bcf97b06218673a6209074bf1b4046575d8e7941
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-08-13 20:01:13 -0400

    Segun la edad, el programa saluda al usuario usando el arquetipo correspondiente

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,7 @@ import bootsaludador from "./sumador";
 
 const first = document.querySelector("#nombre");
 const second = document.querySelector("#genero");
+const tird = document.querySelector("#edad");
 const form = document.querySelector("#saludo-form");
 const div = document.querySelector("#resultado-div");
 
@@ -11,5 +12,5 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const saludo = new bootsaludador();
 
-  div.innerHTML = "<b>"  + "<p>" + saludo.saludar(first.value, second.value) + "</p>"  + "</>";
+  div.innerHTML = "<b>"  + "<p>" + saludo.saludar(first.value, second.value, tird.value) + "</p>"  + "</>";
 });
--- src/sumador.js
+++ src/sumador.js
@@ -1,19 +1,23 @@
 class bootsaludador{
 
-  saludar(nombre, genero) {
-    return  "Hola " + this.verificarGenero(genero) + nombre;
+  saludar(nombre, genero, edad) {
+    return  "Hola " + this.verificarGeneroEdad(genero, edad) + nombre;
   }
 
-  verificarGenero(genero)
+  verificarGeneroEdad(genero, edad)
   {
     genero.toLowerCase();
-    if (genero === "masculino")
+    if (genero === "masculino" && edad >= 30)
       return "Sr. "
-    else 
+    if (genero === "masculino" && edad < 30)
+      return "Joven. "
+    if (genero === "femenino" && edad >= 30)
       return "Sra. "
+    if (genero === "femenino" && edad < 30)
+      return "Señorita. "
   }
 
-  
+
 
 }
 
